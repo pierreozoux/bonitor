7ce5200cf6b2e0a2344d642c156d1412a6fcbef6
pierreozoux <pierre.ozoux@seedrs.com>
2013-10-08 16:53:06 +0100

    Add a link to the messages

--- lib/bonitor/plugins/rss/new_relic_message.rb
+++ lib/bonitor/plugins/rss/new_relic_message.rb
@@ -4,14 +4,16 @@ class NewRelicMessage
   attr_reader :severity
   attr_reader :message
   attr_reader :pretty_date
+  attr_reader :link
 
   def initialize(item)
     @severity = item.title.match(/(?<=\[)[^]]+(?=\])/)
     @message = item.title.match(/(?<=- ).*/)
     @pretty_date = item.pubDate.prettier
+    @link = "https://rpm.newrelic.com" + item.link
   end
 
   def to_s
-    "[#{@severity}] #{@pretty_date}: #{@message}"
+    "[#{@severity}] #{@pretty_date}: #{@message}  ----  For more, please consider visiting #{@link}"
   end
 end
